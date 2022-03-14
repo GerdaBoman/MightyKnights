d534e74b9d6d6857ceac75ed8c0db5165b0c03a5
GerdaBoman <88060352+GerdaBoman@users.noreply.github.com>
2022-03-14 21:52:06 +0100

    commented out MC and Car config to be able to continue testing

--- UI/Forms/FormVehicleManagement.Designer.cs
+++ UI/Forms/FormVehicleManagement.Designer.cs
@@ -65,13 +65,15 @@
             this.newLicancePlateLabel = new System.Windows.Forms.Label();
             this.NewVehicleTypeLabel = new System.Windows.Forms.Label();
             this.ClearAllButton = new System.Windows.Forms.Button();
+            this.DateTimeInstrutions = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // searchLabel
             // 
             this.searchLabel.AutoSize = true;
             this.searchLabel.Font = new System.Drawing.Font("Segoe UI", 15F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Underline))), System.Drawing.GraphicsUnit.Point);
-            this.searchLabel.Location = new System.Drawing.Point(113, 42);
+            this.searchLabel.Location = new System.Drawing.Point(113, 66);
             this.searchLabel.Name = "searchLabel";
             this.searchLabel.Size = new System.Drawing.Size(86, 28);
             this.searchLabel.TabIndex = 0;
@@ -79,14 +81,14 @@
             // 
             // RegNummerTextBox
             // 
-            this.RegNummerTextBox.Location = new System.Drawing.Point(344, 99);
+            this.RegNummerTextBox.Location = new System.Drawing.Point(314, 121);
             this.RegNummerTextBox.Name = "RegNummerTextBox";
             this.RegNummerTextBox.Size = new System.Drawing.Size(170, 23);
             this.RegNummerTextBox.TabIndex = 1;
             // 
             // searchButton
             // 
-            this.searchButton.Location = new System.Drawing.Point(526, 98);
+            this.searchButton.Location = new System.Drawing.Point(520, 121);
             this.searchButton.Name = "searchButton";
             this.searchButton.Size = new System.Drawing.Size(75, 23);
             this.searchButton.TabIndex = 2;
@@ -101,7 +103,7 @@
             this.vehicleType,
             this.ParkingSpot,
             this.CheckInDate});
-            this.searchResultsView.Location = new System.Drawing.Point(129, 165);
+            this.searchResultsView.Location = new System.Drawing.Point(129, 181);
             this.searchResultsView.Name = "searchResultsView";
             this.searchResultsView.Size = new System.Drawing.Size(604, 107);
             this.searchResultsView.TabIndex = 3;
@@ -131,11 +133,11 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(202, 102);
+            this.label1.Location = new System.Drawing.Point(170, 125);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(121, 15);
+            this.label1.Size = new System.Drawing.Size(124, 15);
             this.label1.TabIndex = 4;
-            this.label1.Text = "Licence Plate number";
+            this.label1.Text = "Licence Plate number:";
             // 
             // parkingLotViewer
             // 
@@ -144,7 +146,7 @@
             this.columnHeader2,
             this.columnHeader3,
             this.columnHeader4});
-            this.parkingLotViewer.Location = new System.Drawing.Point(790, 89);
+            this.parkingLotViewer.Location = new System.Drawing.Point(791, 155);
             this.parkingLotViewer.Name = "parkingLotViewer";
             this.parkingLotViewer.Size = new System.Drawing.Size(506, 599);
             this.parkingLotViewer.TabIndex = 5;
@@ -174,26 +176,28 @@
             // ParkedVehiclesLabel
             // 
             this.ParkedVehiclesLabel.AutoSize = true;
-            this.ParkedVehiclesLabel.Font = new System.Drawing.Font("Segoe UI", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.ParkedVehiclesLabel.Location = new System.Drawing.Point(852, 42);
+            this.ParkedVehiclesLabel.Font = new System.Drawing.Font("Segoe UI", 15F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Underline))), System.Drawing.GraphicsUnit.Point);
+            this.ParkedVehiclesLabel.Location = new System.Drawing.Point(791, 116);
             this.ParkedVehiclesLabel.Name = "ParkedVehiclesLabel";
-            this.ParkedVehiclesLabel.Size = new System.Drawing.Size(177, 28);
+            this.ParkedVehiclesLabel.Size = new System.Drawing.Size(197, 28);
             this.ParkedVehiclesLabel.TabIndex = 6;
             this.ParkedVehiclesLabel.Text = "All Parked Vehicles:";
             // 
             // licencePlateLabel
             // 
             this.licencePlateLabel.AutoSize = true;
-            this.licencePlateLabel.Location = new System.Drawing.Point(113, 363);
+            this.licencePlateLabel.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.licencePlateLabel.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.licencePlateLabel.Location = new System.Drawing.Point(101, 362);
             this.licencePlateLabel.Name = "licencePlateLabel";
-            this.licencePlateLabel.Size = new System.Drawing.Size(122, 15);
+            this.licencePlateLabel.Size = new System.Drawing.Size(129, 15);
             this.licencePlateLabel.TabIndex = 7;
             this.licencePlateLabel.Text = "Current Licence Plate:";
             // 
             // VehicleTypeLabel
             // 
             this.VehicleTypeLabel.AutoSize = true;
-            this.VehicleTypeLabel.Location = new System.Drawing.Point(113, 403);
+            this.VehicleTypeLabel.Location = new System.Drawing.Point(101, 402);
             this.VehicleTypeLabel.Name = "VehicleTypeLabel";
             this.VehicleTypeLabel.Size = new System.Drawing.Size(117, 15);
             this.VehicleTypeLabel.TabIndex = 8;
@@ -202,7 +206,7 @@
             // ParkingSpotLabel
             // 
             this.ParkingSpotLabel.AutoSize = true;
-            this.ParkingSpotLabel.Location = new System.Drawing.Point(168, 576);
+            this.ParkingSpotLabel.Location = new System.Drawing.Point(304, 582);
             this.ParkingSpotLabel.Name = "ParkingSpotLabel";
             this.ParkingSpotLabel.Size = new System.Drawing.Size(77, 30);
             this.ParkingSpotLabel.TabIndex = 9;
@@ -211,11 +215,11 @@
             // CheckInDateLabel
             // 
             this.CheckInDateLabel.AutoSize = true;
-            this.CheckInDateLabel.Location = new System.Drawing.Point(113, 444);
+            this.CheckInDateLabel.Location = new System.Drawing.Point(101, 444);
             this.CheckInDateLabel.Name = "CheckInDateLabel";
-            this.CheckInDateLabel.Size = new System.Drawing.Size(83, 15);
+            this.CheckInDateLabel.Size = new System.Drawing.Size(84, 15);
             this.CheckInDateLabel.TabIndex = 10;
-            this.CheckInDateLabel.Text = "Check In Date:";
+            this.CheckInDateLabel.Text = "Arrival Time * :";
             // 
             // editRegNumber
             // 
@@ -280,16 +284,16 @@
             // 
             // editParkingSpot
             // 
-            this.editParkingSpot.Location = new System.Drawing.Point(275, 583);
+            this.editParkingSpot.Location = new System.Drawing.Point(411, 589);
             this.editParkingSpot.Name = "editParkingSpot";
-            this.editParkingSpot.Size = new System.Drawing.Size(307, 23);
+            this.editParkingSpot.Size = new System.Drawing.Size(73, 23);
             this.editParkingSpot.TabIndex = 13;
             // 
             // moveVehicleLabel
             // 
             this.moveVehicleLabel.AutoSize = true;
             this.moveVehicleLabel.Font = new System.Drawing.Font("Segoe UI", 15F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Underline))), System.Drawing.GraphicsUnit.Point);
-            this.moveVehicleLabel.Location = new System.Drawing.Point(113, 522);
+            this.moveVehicleLabel.Location = new System.Drawing.Point(113, 529);
             this.moveVehicleLabel.Name = "moveVehicleLabel";
             this.moveVehicleLabel.Size = new System.Drawing.Size(144, 28);
             this.moveVehicleLabel.TabIndex = 22;
@@ -297,7 +301,7 @@
             // 
             // MoveVehicleButton
             // 
-            this.MoveVehicleButton.Location = new System.Drawing.Point(598, 634);
+            this.MoveVehicleButton.Location = new System.Drawing.Point(520, 615);
             this.MoveVehicleButton.Name = "MoveVehicleButton";
             this.MoveVehicleButton.Size = new System.Drawing.Size(135, 23);
             this.MoveVehicleButton.TabIndex = 23;
@@ -317,7 +321,7 @@
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(165, 638);
+            this.label2.Location = new System.Drawing.Point(301, 644);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(104, 15);
             this.label2.TabIndex = 25;
@@ -325,9 +329,9 @@
             // 
             // newParkingSpot
             // 
-            this.newParkingSpot.Location = new System.Drawing.Point(275, 634);
+            this.newParkingSpot.Location = new System.Drawing.Point(411, 640);
             this.newParkingSpot.Name = "newParkingSpot";
-            this.newParkingSpot.Size = new System.Drawing.Size(307, 23);
+            this.newParkingSpot.Size = new System.Drawing.Size(73, 23);
             this.newParkingSpot.TabIndex = 26;
             // 
             // NewVehicleType
@@ -368,13 +372,36 @@
             // 
             // ClearAllButton
             // 
-            this.ClearAllButton.Location = new System.Drawing.Point(632, 98);
+            this.ClearAllButton.Location = new System.Drawing.Point(625, 120);
             this.ClearAllButton.Name = "ClearAllButton";
             this.ClearAllButton.Size = new System.Drawing.Size(75, 23);
             this.ClearAllButton.TabIndex = 31;
-            this.ClearAllButton.Text = "Clear";
+            this.ClearAllButton.Text = "Clear All";
             this.ClearAllButton.UseVisualStyleBackColor = true;
             this.ClearAllButton.Click += new System.EventHandler(this.ClearAllButton_Click);
+            // 
+            // DateTimeInstrutions
+            // 
+            this.DateTimeInstrutions.AutoSize = true;
+            this.DateTimeInstrutions.Location = new System.Drawing.Point(241, 466);
+            this.DateTimeInstrutions.Name = "DateTimeInstrutions";
+            this.DateTimeInstrutions.Size = new System.Drawing.Size(347, 15);
+            this.DateTimeInstrutions.TabIndex = 32;
+            this.DateTimeInstrutions.Text = "* Select date from calender and enter time to change arrival time\r\n";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Segoe UI", 13F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Underline))), System.Drawing.GraphicsUnit.Point);
+            this.label3.ForeColor = System.Drawing.Color.Firebrick;
+            this.label3.Location = new System.Drawing.Point(231, 18);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(914, 50);
+            this.label3.TabIndex = 33;
+            this.label3.Text = "Enter vehicles licence plate number into \"Current Licence Plate\" to be able to up" +
+    "date vehicle informaition!\r\n Search the licence plate number to have all informa" +
+    "tion entered automaticly!";
+            this.label3.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
             // FormVehicleManagement
             // 
@@ -382,6 +409,8 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.LightGray;
             this.ClientSize = new System.Drawing.Size(1342, 777);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.DateTimeInstrutions);
             this.Controls.Add(this.ClearAllButton);
             this.Controls.Add(this.NewVehicleTypeLabel);
             this.Controls.Add(this.newLicancePlateLabel);
@@ -458,5 +487,7 @@
         private Label newLicancePlateLabel;
         private Label NewVehicleTypeLabel;
         private Button ClearAllButton;
+        private Label DateTimeInstrutions;
+        private Label label3;
     }
 }
