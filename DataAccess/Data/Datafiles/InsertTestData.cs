dd1e341488aaef071ca0430838fd94b7fb42ebc7
GerdaBoman <88060352+GerdaBoman@users.noreply.github.com>
2022-03-15 21:31:59 +0100

    history ttestdata

--- DataAccess/Data/Datafiles/InsertTestData.cs
+++ DataAccess/Data/Datafiles/InsertTestData.cs
@@ -50,11 +50,20 @@ namespace DataAccess.Data.Datafiles
                 db.ParkingLots.Add(new ParkingLot { VehicleId = 14, SpotNumber = 21, Arrival = DateTime.Parse("2022/03/01 12:23:22"), Departure = null });
                 db.ParkingLots.Add(new ParkingLot { VehicleId = 15, SpotNumber = 56, Arrival = DateTime.Parse("2022/03/04 01:53:22"), Departure = null });
                 db.ParkingLots.Add(new ParkingLot { VehicleId = 16, SpotNumber = 85, Arrival = DateTime.Parse("2022/03/12 12:12:22"), Departure = null });
-                db.ParkingLots.Add(new ParkingLot { VehicleId = 17, SpotNumber = 21, Arrival = DateTime.Parse("2022/02/21 14:14:22"), Departure = null });
+                db.ParkingLots.Add(new ParkingLot { VehicleId = 17, SpotNumber = 30, Arrival = DateTime.Parse("2022/02/21 14:14:22"), Departure = null });
                 db.ParkingLots.Add(new ParkingLot { VehicleId = 18, SpotNumber = 10, Arrival = DateTime.Parse("2022/03/11 15:13:22"), Departure = null });
                 db.ParkingLots.Add(new ParkingLot { VehicleId = 19, SpotNumber = 28, Arrival = DateTime.Parse("2022/03/13 16:21:22"), Departure = null });
                 db.ParkingLots.Add(new ParkingLot { VehicleId = 20, SpotNumber = 30, Arrival = DateTime.Parse("2022/03/14 18:42:22"), Departure = null });
                 db.ParkingLots.Add(new ParkingLot { VehicleId = 21, SpotNumber = 15, Arrival = DateTime.Parse("2022/03/11 14:23:22"), Departure = null });
+
+                db.SaveChanges();
+
+                db.Histories.Add(new History { LicencePlate = "gresw123", ParkingSpot = 23, ArrivalDate = DateTime.Parse("2022/02/12 12:43:31"), DepartureDate = DateTime.Parse("2022/03/11 14:23:22"), TotalCost = 25353.23m });
+                db.Histories.Add(new History { LicencePlate = "233fsfg", ParkingSpot = 12, ArrivalDate = DateTime.Parse("2022/03/15 13:41:15"), DepartureDate = DateTime.Parse("2022/03/15 12:43:12"), TotalCost = 42.54m });
+                db.Histories.Add(new History { LicencePlate = "george123", ParkingSpot = 43, ArrivalDate = DateTime.Parse("2022/03/15 15:33:31"), DepartureDate = DateTime.Parse("2022/03/15 18:23:22"), TotalCost = 80.43m });
+                db.Histories.Add(new History { LicencePlate = "xde234", ParkingSpot = 16, ArrivalDate = DateTime.Parse("2022/03/12 13:13:31"), DepartureDate = DateTime.Parse("2022/03/13 08:23:12"), TotalCost = 1780.32m });
+                db.Histories.Add(new History { LicencePlate = "123dwe", ParkingSpot = 75, ArrivalDate = DateTime.Parse("2022/03/14 12:54:12"), DepartureDate = DateTime.Parse("2022/03/14 14:23:22"), TotalCost = 32.54m });
+                db.Histories.Add(new History { LicencePlate = "53fdex", ParkingSpot = 31, ArrivalDate = DateTime.Parse("2022/03/11 09:15:42"), DepartureDate = DateTime.Parse("2022/03/11 11:22:23"), TotalCost = 35.43m });
 
                 db.SaveChanges();
             }
