10b7006dbc4cdc9efa8b2091accc6456370a1813
GerdaBoman <88060352+GerdaBoman@users.noreply.github.com>
2022-03-15 16:24:10 +0100

    FormSetting info information + Warning

--- UI/Forms/FormSettings.Designer.cs
+++ UI/Forms/FormSettings.Designer.cs
@@ -28,7 +28,6 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.label1 = new System.Windows.Forms.Label();
             this.edit_btn = new System.Windows.Forms.Button();
             this.carPrice_lbl = new System.Windows.Forms.Label();
             this.mcPrice_lbl = new System.Windows.Forms.Label();
@@ -37,24 +36,19 @@
             this.mcPrice_txt = new System.Windows.Forms.TextBox();
             this.gSize_txt = new System.Windows.Forms.TextBox();
             this.Savebtn = new System.Windows.Forms.Button();
+            this.infoLabel = new System.Windows.Forms.Label();
+            this.iconPictureBox1 = new FontAwesome.Sharp.IconPictureBox();
+            this.iconPictureBox2 = new FontAwesome.Sharp.IconPictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.iconPictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.iconPictureBox2)).BeginInit();
             this.SuspendLayout();
-            // 
-            // label1
-            // 
-            this.label1.Anchor = System.Windows.Forms.AnchorStyles.None;
-            this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("Segoe UI", 30F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
-            this.label1.Location = new System.Drawing.Point(406, 52);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(286, 54);
-            this.label1.TabIndex = 1;
-            this.label1.Text = "Settings Form";
             // 
             // edit_btn
             // 
-            this.edit_btn.Location = new System.Drawing.Point(13, 219);
+            this.edit_btn.Location = new System.Drawing.Point(488, 409);
             this.edit_btn.Name = "edit_btn";
-            this.edit_btn.Size = new System.Drawing.Size(94, 29);
+            this.edit_btn.Size = new System.Drawing.Size(97, 54);
             this.edit_btn.TabIndex = 2;
             this.edit_btn.Text = "Edit";
             this.edit_btn.UseVisualStyleBackColor = true;
@@ -63,40 +57,40 @@
             // carPrice_lbl
             // 
             this.carPrice_lbl.AutoSize = true;
-            this.carPrice_lbl.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
-            this.carPrice_lbl.Location = new System.Drawing.Point(10, 207);
+            this.carPrice_lbl.Font = new System.Drawing.Font("Segoe UI", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.carPrice_lbl.Location = new System.Drawing.Point(490, 154);
             this.carPrice_lbl.Name = "carPrice_lbl";
-            this.carPrice_lbl.Size = new System.Drawing.Size(77, 21);
+            this.carPrice_lbl.Size = new System.Drawing.Size(96, 28);
             this.carPrice_lbl.TabIndex = 5;
             this.carPrice_lbl.Text = "Car Price";
             // 
             // mcPrice_lbl
             // 
             this.mcPrice_lbl.AutoSize = true;
-            this.mcPrice_lbl.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
-            this.mcPrice_lbl.Location = new System.Drawing.Point(11, 272);
+            this.mcPrice_lbl.Font = new System.Drawing.Font("Segoe UI", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.mcPrice_lbl.Location = new System.Drawing.Point(492, 237);
             this.mcPrice_lbl.Name = "mcPrice_lbl";
-            this.mcPrice_lbl.Size = new System.Drawing.Size(75, 21);
+            this.mcPrice_lbl.Size = new System.Drawing.Size(94, 28);
             this.mcPrice_lbl.TabIndex = 6;
             this.mcPrice_lbl.Text = "Mc Price";
             // 
             // garageSize_lbl
             // 
             this.garageSize_lbl.AutoSize = true;
-            this.garageSize_lbl.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
-            this.garageSize_lbl.Location = new System.Drawing.Point(11, 338);
+            this.garageSize_lbl.Font = new System.Drawing.Font("Segoe UI", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.garageSize_lbl.Location = new System.Drawing.Point(488, 314);
             this.garageSize_lbl.Name = "garageSize_lbl";
-            this.garageSize_lbl.Size = new System.Drawing.Size(99, 21);
+            this.garageSize_lbl.Size = new System.Drawing.Size(123, 28);
             this.garageSize_lbl.TabIndex = 7;
             this.garageSize_lbl.Text = "Garage Size";
             // 
             // cPrice_txt
             // 
             this.cPrice_txt.BorderStyle = System.Windows.Forms.BorderStyle.None;
-            this.cPrice_txt.Location = new System.Drawing.Point(155, 281);
+            this.cPrice_txt.Location = new System.Drawing.Point(633, 165);
             this.cPrice_txt.Name = "cPrice_txt";
             this.cPrice_txt.ReadOnly = true;
-            this.cPrice_txt.Size = new System.Drawing.Size(72, 20);
+            this.cPrice_txt.Size = new System.Drawing.Size(97, 16);
             this.cPrice_txt.TabIndex = 8;
             this.cPrice_txt.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
             this.cPrice_txt.ReadOnlyChanged += new System.EventHandler(this.edit_btn_Click);
@@ -104,32 +98,84 @@
             // mcPrice_txt
             // 
             this.mcPrice_txt.BorderStyle = System.Windows.Forms.BorderStyle.None;
-            this.mcPrice_txt.Location = new System.Drawing.Point(155, 365);
+            this.mcPrice_txt.Location = new System.Drawing.Point(633, 243);
             this.mcPrice_txt.Name = "mcPrice_txt";
             this.mcPrice_txt.ReadOnly = true;
-            this.mcPrice_txt.Size = new System.Drawing.Size(72, 20);
+            this.mcPrice_txt.Size = new System.Drawing.Size(97, 16);
             this.mcPrice_txt.TabIndex = 9;
             this.mcPrice_txt.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
             // 
             // gSize_txt
             // 
             this.gSize_txt.BorderStyle = System.Windows.Forms.BorderStyle.None;
-            this.gSize_txt.Location = new System.Drawing.Point(155, 455);
+            this.gSize_txt.Location = new System.Drawing.Point(633, 325);
             this.gSize_txt.Name = "gSize_txt";
             this.gSize_txt.ReadOnly = true;
-            this.gSize_txt.Size = new System.Drawing.Size(72, 20);
+            this.gSize_txt.Size = new System.Drawing.Size(97, 16);
             this.gSize_txt.TabIndex = 10;
             this.gSize_txt.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
             // 
             // Savebtn
             // 
-            this.Savebtn.Location = new System.Drawing.Point(155, 219);
+            this.Savebtn.Location = new System.Drawing.Point(633, 409);
             this.Savebtn.Name = "Savebtn";
-            this.Savebtn.Size = new System.Drawing.Size(49, 29);
+            this.Savebtn.Size = new System.Drawing.Size(97, 54);
             this.Savebtn.TabIndex = 11;
             this.Savebtn.Text = "Save";
             this.Savebtn.UseVisualStyleBackColor = true;
             this.Savebtn.Click += new System.EventHandler(this.Savebtn_Click);
+            // 
+            // infoLabel
+            // 
+            this.infoLabel.AutoSize = true;
+            this.infoLabel.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.infoLabel.Font = new System.Drawing.Font("Segoe UI", 13F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.infoLabel.Location = new System.Drawing.Point(181, 62);
+            this.infoLabel.Name = "infoLabel";
+            this.infoLabel.Size = new System.Drawing.Size(195, 102);
+            this.infoLabel.TabIndex = 12;
+            this.infoLabel.Text = "To edit configurations: \r\n1.Press Edit\r\n2.Change prices\r\n3.Press Save\r\n";
+            // 
+            // iconPictureBox1
+            // 
+            this.iconPictureBox1.BackColor = System.Drawing.Color.LightGray;
+            this.iconPictureBox1.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.iconPictureBox1.IconChar = FontAwesome.Sharp.IconChar.Question;
+            this.iconPictureBox1.IconColor = System.Drawing.SystemColors.ControlText;
+            this.iconPictureBox1.IconFont = FontAwesome.Sharp.IconFont.Auto;
+            this.iconPictureBox1.IconSize = 102;
+            this.iconPictureBox1.Location = new System.Drawing.Point(73, 62);
+            this.iconPictureBox1.Name = "iconPictureBox1";
+            this.iconPictureBox1.Size = new System.Drawing.Size(102, 102);
+            this.iconPictureBox1.TabIndex = 13;
+            this.iconPictureBox1.TabStop = false;
+            // 
+            // iconPictureBox2
+            // 
+            this.iconPictureBox2.BackColor = System.Drawing.Color.LightGray;
+            this.iconPictureBox2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.iconPictureBox2.IconChar = FontAwesome.Sharp.IconChar.ExclamationTriangle;
+            this.iconPictureBox2.IconColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.iconPictureBox2.IconFont = FontAwesome.Sharp.IconFont.Auto;
+            this.iconPictureBox2.IconSize = 102;
+            this.iconPictureBox2.Location = new System.Drawing.Point(646, 544);
+            this.iconPictureBox2.Name = "iconPictureBox2";
+            this.iconPictureBox2.Size = new System.Drawing.Size(102, 102);
+            this.iconPictureBox2.TabIndex = 14;
+            this.iconPictureBox2.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 13F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label1.Location = new System.Drawing.Point(764, 524);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(574, 152);
+            this.label1.TabIndex = 15;
+            this.label1.Text = "Things to think about:\r\n\r\n- If you are decreasing parking lot capacity:          " +
+    "            \r\n Make sure no vehicles are parked in those spaces that will be rem" +
+    "oved\r\nto make sure no loss of data!\r\n ";
             // 
             // FormSettings
             // 
@@ -137,6 +183,10 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.LightGray;
             this.ClientSize = new System.Drawing.Size(1534, 1036);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.iconPictureBox2);
+            this.Controls.Add(this.iconPictureBox1);
+            this.Controls.Add(this.infoLabel);
             this.Controls.Add(this.Savebtn);
             this.Controls.Add(this.gSize_txt);
             this.Controls.Add(this.mcPrice_txt);
@@ -145,18 +195,17 @@
             this.Controls.Add(this.mcPrice_lbl);
             this.Controls.Add(this.carPrice_lbl);
             this.Controls.Add(this.edit_btn);
-            this.Controls.Add(this.label1);
             this.Name = "FormSettings";
             this.Text = "Settings";
             this.Load += new System.EventHandler(this.FormSettings_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.iconPictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.iconPictureBox2)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
         }
 
         #endregion
-
-        private Label label1;
         private Button edit_btn;
         private Label carPrice_lbl;
         private Label mcPrice_lbl;
@@ -165,5 +214,9 @@
         private TextBox mcPrice_txt;
         private TextBox gSize_txt;
         private Button Savebtn;
+        private Label infoLabel;
+        private FontAwesome.Sharp.IconPictureBox iconPictureBox1;
+        private FontAwesome.Sharp.IconPictureBox iconPictureBox2;
+        private Label label1;
     }
 }
